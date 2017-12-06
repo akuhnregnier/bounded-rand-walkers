Fixed normalisation of g. Also improved the function minimum finding speed for the random walker.

comparisons.py
@@ -9,7 +9,7 @@ import os
 import matplotlib.pyplot as plt
 import numpy as np
 from rotation_steps import (g1D, gRadialCircle, Pdf_Transform,
-                            rot_steps, g1D_norm)
+                            rot_steps, g1D_norm, g2D)
 from functions import Tophat_1D, Tophat_2D, Power, Exponential, Gaussian, Funky
 import scipy.integrate
 from data_generation import multi_random_walker, circle_points, weird_bounds
@@ -145,31 +145,8 @@ def compare_2D(pdf, nr_bins, num_samples=int(1e4),
     x_centres = get_centres(x_edges)
     y_centres = get_centres(y_edges)
     xcoords, ycoords = np.meshgrid(x_centres, y_centres)
-    rads = np.sqrt(xcoords**2. + ycoords**2.)
-    unique_rads = np.unique(rads)
 
-    total_mask = np.zeros_like(rads, dtype=bool)
-
-    g_analytical = np.zeros_like(rads)
-
-    nr_unique_rads = len(unique_rads)
-    logger.info('integrating for {:} unique radii'
-                .format(len(unique_rads)))
-
-    for i, rad in enumerate(unique_rads):
-        logger.info('{:>5d} out of {:>5d}'
-                    .format(i + 1, nr_unique_rads))
-        g_analytical_value = gRadialCircle(rad, pdf)
-        mask = np.where(np.isclose(rad, rads))
-        if not np.isnan(g_analytical_value):
-            total_mask |= np.isclose(rad, rads)
-        g_analytical[mask] = g_analytical_value
-
-    g_analytical /= scipy.integrate.quad(
-            lambda x: (2*np.pi*x) * gRadialCircle(x, pdf), 0, 1
-            )[0]
-
-    g_analytical[~total_mask] = 0.
+    g_analytical = g2D(pdf, x_edges, y_edges, bounds)
 
     ft_xs = np.linspace(-2, 2, nr_bins + 1, endpoint=True)
     ft_ys = np.linspace(-2, 2, nr_bins + 1, endpoint=True)
@@ -197,8 +174,8 @@ def compare_2D(pdf, nr_bins, num_samples=int(1e4),
     in this case, the cells have equal areas
     """
     cell_area = np.abs((ft_xs[1] - ft_xs[0]) * (ft_ys[1] - ft_ys[0]))
-    total_integral = np.sum(f_t_analytical) * cell_area
-    f_t_analytical /= total_integral
+    total_p = np.sum(f_t_analytical) * cell_area
+    f_t_analytical /= total_p
 
     logger.info('Finished analytical result, starting numerical run')
 
@@ -214,6 +191,15 @@ def compare_2D(pdf, nr_bins, num_samples=int(1e4),
     g_numerical, _, _ = np.histogram2d(
             *positions.T, bins=[x_edges, y_edges],
             normed=True
+            )
+
+    # normalise g_numerical
+    g_mask = np.isclose(g_numerical, 0)  # avoid dividing by 0
+    g_numerical[~g_mask] /= np.sum(
+            (g_numerical
+             * ((x_edges[1:] - x_edges[:-1]).reshape(-1, 1))
+                * (y_edges[1:] - y_edges[:-1]).reshape(1, -1)
+             )[~g_mask]
             )
 
     f_t_numerical, _, _ = np.histogram2d(
@@ -441,8 +427,8 @@ def compare_2D_plotting(pdf, nr_bins, steps=int(1e3),
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
-    ONE_D = True
-    TWO_D = False
+    ONE_D = False
+    TWO_D = True
 
     if ONE_D:
         # 1D case
@@ -468,7 +454,7 @@ if __name__ == '__main__':
                     'width': 2.
                     }),
                 ]
-        bins = 51
+        bins = 81
         for PDFClass, pdf_name, kwargs in pdfs_args_2D:
             pdf = PDFClass(**kwargs).pdf
 

data_generation.py
@@ -95,7 +95,7 @@ def generate_random_samples(f_i, position, nr_samples, dimensions=1):
         max_fn = generate_random_samples.max_fn_value
         logger.debug('Got:{:}'.format(max_fn))
     else:
-        logger.debug('Finding maximum of f_i')
+        logger.info('Finding maximum of f_i')
         # need to be careful, since the minimiser might never find the
         # minimum if the function is like a tophat function - ie. if it is
         # flat in some regions!
@@ -112,7 +112,7 @@ def generate_random_samples(f_i, position, nr_samples, dimensions=1):
             # look at the function value at many positions and then find
             # the minimum around the minimum of the points discovered so
             # far
-            N = 1000
+            N = 100
             trial_coords = np.linspace(-1, 1, N)
             fn_values = np.zeros((N, N), dtype=np.float64)
             for i in range(N):
@@ -128,7 +128,7 @@ def generate_random_samples(f_i, position, nr_samples, dimensions=1):
                                            )
             max_fn = f_i(*max_args)
         # fix this value so it does not need to be calculated next time
-        logger.debug('Fixing maximum fn')
+        logger.info('storing maximum fn value')
         generate_random_samples.max_fn_value = max_fn
         logger.debug('max_fn:{:}'.format(generate_random_samples.max_fn_value))
 
@@ -415,7 +415,7 @@ def multi_random_walker(n_processes, f_i, bounds, steps=int(1e2)):
                 )
     def rand_walk_worker(procnum, return_dict):
         """Worker function which executes the random_walker"""
-        print(str(procnum) + ' present!')
+        print('process ' + str(procnum) + ' has started!')
         step_values, positions = random_walker(
                 f_i=f_i,
                 bounds=bounds,

rotation_steps.py
@@ -168,10 +168,10 @@ def g2D(f, xs_edges, ys_edges, bounds=weird_bounds):
         # positions. But only iterate over the positions that are
         # actually in the boundary.
         if max_counter < 20:
-            print('counter:', counter, 'out of:', max_counter)
+            print('counter:', counter + 1, 'out of:', max_counter)
         else:
             if ((counter) % (max_counter / 10)) == 0:
-                print('{:06.2%}'.format(float(counter) / max_counter))
+                print('{:07.2%}'.format(float(counter) / (max_counter - 1)))
         counter += 1
         x, y = (xs_centres[mask_x_index],
                 ys_centres[mask_y_index])
@@ -183,6 +183,13 @@ def g2D(f, xs_edges, ys_edges, bounds=weird_bounds):
                                  y_mod[mask_y_index])
             g_values[mask_x_index, mask_y_index] += f(*relative_position)
 
+    cell_areas = ((xs_edges[1:] - xs_edges[:-1]).reshape(-1, 1)
+                  * (ys_edges[1:] - ys_edges[:-1]).reshape(1, -1)
+                  )
+    total_prob = cell_areas * g_values
+    g_mask = np.isclose(g_values, 0)
+    # return the normalised g values
+    g_values[~g_mask] /= np.sum(total_prob[~g_mask])
     return g_values
 
 